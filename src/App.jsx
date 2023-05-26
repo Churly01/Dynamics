979fbddae0080188cebc95734c59a1ac321decd5
churly <nietopetinalcarlos@gmail.com>
2023-05-26 16:16:04 +0200

    Add socket testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import MainView            from './MainView.jsx';
 import NavBar              from './NavBar.jsx';
 import LocalDynamic    from './LocalDynamic.jsx';
 import { useEffect, useState } from "react";
-import {io} from "socket.io-client";
-
-const SOCKETIO_URL = import.meta.env.SOCKET_IO_URL || "localhost:4001";
-const socket = io(SOCKETIO_URL);
+import socket from "./socket.js";
 
 function App() {
   const [response, setResponse] = useState("");
+
   useEffect(() => {
     socket.on("connect", () => {
       console.log("connected");
@@ -25,10 +23,6 @@ function App() {
       <div style={{fontSize: '3em', marginTop:'0.5em'}}>
         Survival on the Moon
       </div>
-      <div style={{fontSize: '1.5em', marginTop:'0.5em'}}>
-        {response}
-      </div>
-
       <Routes>
         <Route path="/" exact element={<MainView/>}/>
         <Route path="/local_dynamic"  element={<LocalDynamic/>}/>
---
+++ src/socket.js
@@ -0,0 +1,5 @@
+import {io} from "socket.io-client";
+const SOCKETIO_URL = import.meta.env.SOCKET_IO_URL || "localhost:4001";
+const socket = io(SOCKETIO_URL);
+
+export default socket;
