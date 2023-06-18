8ac7bc4d0350a6858bd3298ae9d41f993c8534c6
churly <nietopetinalcarlos@gmail.com>
2023-06-18 22:09:31 +0200

    Remove useless stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import {Route, Routes} from "react-router-dom";
-import MainView            from './MainView.jsx';
-import NavBar              from './NavBar.jsx';
-import LocalDynamic    from './LocalDynamic/LocalDynamic.jsx';
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import MainView from './MainView.jsx';
+import NavBar from './NavBar.jsx';
+import LocalDynamic from './LocalDynamic/LocalDynamic.jsx';
 
 function App() {
-  const [response, setResponse] = useState("");
+  document.title = "Survival on the Moon";
 
   return (
     <>
-      <NavBar/>
-      <div style={{fontSize: '3em', marginTop:'0.5em'}}>
+      <NavBar />
+      <div style={{ fontSize: '3em', marginTop: '0.5em' }}>
         Survival on the Moon
       </div>
       <Routes>
-        <Route path="/" exact element={<MainView/>}/>
-        <Route path="/local_dynamic"  element={<LocalDynamic/>}/>
+        <Route path="/" exact element={<MainView />} />
+        <Route path="/local_dynamic" element={<LocalDynamic />} />
       </Routes>
     </>
   );
--- src/MainView.jsx
+++ src/MainView.jsx
@@ -1,49 +1,18 @@
 import { Link } from "react-router-dom";
-import socket from "./socket";
-import { useEffect, useState } from "react";
 
-const  MainView = () => {
-  const [value, setValue] = useState("");
-  const [messages, setMessages] = useState([]);
+const MainView = () => {
 
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected");
-    });
-    socket.on("chat message", (msg) => {
-      setMessages((messages) => [...messages, msg]);
-    });
-
-    return () => {
-      socket.off("chat message");
-      socket.off("connect");
-    };
-  }, []);
   return (
     <>
-      <div style={{fontSize: '2em', textAlign:'justify', margin:'0.5em'}}>
+      <div style={{ fontSize: '2em', textAlign: 'justify', margin: '0.5em' }}>
         You are a member of a space crew originally scheduled to rendezvous with a mother ship on the lighted surface of the moon. However, due to mechanical difficulties, your ship was forced to land at a spot some 200 miles from the rendezvous point. During reentry and landing, much of the equipment aboard was damaged and, since survival depends on reaching the mother ship, the most critical items available must be chosen for the 200-mile trip. Below are listed the 15 items left intact and undamaged after landing. Your task is to rank order them in terms of their importance for your crew in allowing them to reach the rendezvous point. Place the number 1 by the most important item, the number 2 by the second most important, and so on through number 15 for the least important.
       </div>
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      >
-      </input>
-      <button
-        onClick={() => {
-          socket.emit("chat message", value);
-        }}
-      >
-        Send Message
-      </button>
       <button>
-        <Link to="/local_dynamic">Start Dynamic</Link>
+        <Link to="/local_dynamic">
+          Start Locally<br />
+          and Individually
+        </Link>
       </button>
-      <ul>
-        {messages.map((msg, i) => (
-          <li key={i}>{msg}</li>
-        ))}
-      </ul>
     </>
   );
 };
