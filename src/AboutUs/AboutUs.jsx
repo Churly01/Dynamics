640c21d3d74b9d1b6f070dcdb46efc1fe6dfad3c
yana <yana.gizdova1@gmail.com>
2023-06-19 21:18:11 +0200

    Update AboutUs.jsx

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -2,5 +2,21 @@
 
 // Cambiar en esta carpeta, podeis anyadir los componentes que hagan falta
 export default function AboutUs() {
-  return <>placeholder para la vista de about us</>
+  document.title = "About Us";
+  return <>
+    <h1>About Us</h1>
+    <p>Survival on the Moon is a web page representation of NASA's group dynamic about survival on the Moon.</p>
+    <p>For this, each group must make decisions to survive on the Moon.</p>
+    <p>These decisions are made based on the resources available and the events that occur.</p>
+    <p>The final purpose of each participant is to order the list of resources in order of importance from 1 to 15.</p>
+    <p>Once the group has finished the dynamic, the page will show the results of the group and the results of the rest of the groups.</p>
+    <p>This page was developed by 4 students from the Polytechnic University of Madrid.</p>
+    <p>For more information about the project, you can contact us at the following linkIn addresses:</p>"
+    <ul>
+      <li>Carlos Nieto Petinal: <a href="https://www.linkedin.com/in/carlosnietopetinal/">https://www.linkedin.com/in/carlosnietopetinal/</a></li>
+      <li>César Agulló Quirós: <a href="https://www.linkedin.com/in/c%C3%A9sar-agull%C3%B3-a299a622b/">https://www.linkedin.com/in/c%C3%A9sar-agull%C3%B3-a299a622b/</a></li>
+      <li>Sara Rodríguez Rojo: <a href="https://www.linkedin.com/in/sara-rodriguez-rojo/">https://www.linkedin.com/in/sara-rodriguez-rojo/</a></li>
+      <li>Yana Nikolaeva Gizdova: <a href="https://www.linkedin.com/in/yana-nikolaeva-gizdova/">https://www.linkedin.com/in/yana-nikolaeva-gizdova/</a></li>
+    </ul>
+  </>
 }
